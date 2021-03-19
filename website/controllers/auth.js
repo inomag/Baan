ac3186264a5c8cb8f063d15c023f91a96b8ab964
Bucephalus <bhargabnath691@gmail.com>
2021-03-20 01:59:02 +0530

    verify OTP for signup feature added

--- website/controllers/auth.js
+++ website/controllers/auth.js
@@ -5,6 +5,13 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = user => {
+    return jwt.sign({ user }, process.env.JWT_SECRET, {
+        expiresIn: maxAge
+    });
+};
+
 exports.phone_signup = async (req, res) => {
     try {
         console.log(req.body);
@@ -21,5 +28,54 @@ exports.phone_signup = async (req, res) => {
     } catch (err) {
         console.log(err);
         return res.json({ error: 'Some error occured!!!', err });
+    }
+};
+
+exports.verifyOTP = async (req, res) => {
+    try {
+        console.log(req.body);
+        const { phone, code, name, default_loc } = req.body;
+        const data = await client.verify.services('VAc9f4c6e99c3103c020c574b42ca391a6').verificationChecks.create({
+            to: `+91${phone}`,
+            code
+        });
+
+        if (data.status === 'approved') {
+
+            const user = await User.findOne({ phone });
+
+            if (user) {
+                const token = await createToken(user);
+
+                res.cookie('INUNDATION', token, { httpOnly: true, maxAge: maxAge * 1000 });
+
+                return res.status(400).json({
+                    user,
+                    error: 'This phone is already registered!'
+                });
+            } else {
+                const newUser = new User({
+                    phone,
+                    name,
+                    default_loc
+                });
+                await newUser.save();
+
+                const token = await createToken(newUser);
+                res.cookie('INUNDATION', token, { httpOnly: true, maxAge: maxAge * 1000 });
+
+                return res.json({
+                    user: newUser,
+                    success: 'User authenticated successfully!'
+                });
+            }
+        } else {
+            return res.json({
+                error: 'Please input the correct OTP!'
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.json({ error: 'Some error occured!' });
     }
 };
--- website/routes/auth.js
+++ website/routes/auth.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const { phone_signup } = require('../controllers/auth');
+const { phone_signup, verifyOTP } = require('../controllers/auth');
 
 //SIGNUP
+//Send OTP
 router.post('/signup', phone_signup);
+//Verify OTP 
+router.post('/verifyOTP', verifyOTP);
 
 module.exports = router;
