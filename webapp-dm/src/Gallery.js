2798bf3d03f9b6654a6d62516762dd6b3dfef9c5
abinashstack <abinash.gogoi55@gmail.com>
2021-03-20 15:55:07 +0530

    featuree removed

--- webapp-dm/src/Gallery.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import Gallery from "react-photo-gallery";
-import { photos } from "./photos";
-
-/* popout the browser and maximize to see more rows! -> */
-export const BasicRows = () => <Gallery photos={photos} />;
-render(<BasicRows />, document.getElementById("app"));
