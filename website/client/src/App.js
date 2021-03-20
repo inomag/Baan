48c38a84fece18041f76ef25b89d4221156c7e38
Bucephalus <bhargabnath691@gmail.com>
2021-03-20 19:22:43 +0530

    home page and navbar added

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -1,10 +1,19 @@
+import { Fragment } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="App">
-      <h2>Hello Dear!</h2>
-    </div>
+    <Fragment>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
---
+++ website/client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Home = () => {
+    return (
+        <Fragment>
+            <div className="container">
+
+            </div>
+        </Fragment>
+    );
+}
+
+export default Home;
