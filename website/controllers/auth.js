8d0b7e1eff898599bcf44c1714ba8c2e42aa614f
Bucephalus <bhargabnath691@gmail.com>
2021-03-20 15:26:13 +0530

    admin signup routes added

--- website/controllers/auth.js
+++ website/controllers/auth.js
@@ -98,3 +98,71 @@ exports.get_user_by_phone_no = async (req, res) => {
     const user = await User.findOne({ phone: req.params.phone });
     res.json({ user });
 }
+
+exports.admin_phone_signup = async (req, res) => {
+    try {
+        console.log(req.body);
+        const data = await client.verify.services(process.env.TWILIO_SERVICE_ID).verifications.create({
+            to: `+91${req.body.phone}`,
+            channel: 'sms'
+        });
+
+        if (data.status === 'pending') {
+            res.json({ success: 'Please check your inbox for OTP!' });
+        } else {
+            return res.json({ error: 'Sorry! Please input a valid phone number!' });
+        }
+    } catch (err) {
+        return res.json({ error: 'Some error occured!!!', err });
+    }
+};
+
+exports.admin_otp_verify = async (req, res) => {
+    try {
+        console.log(req.body);
+        const { phone, code, name, default_loc, role } = req.body;
+        const data = await client.verify.services(process.env.TWILIO_SERVICE_ID).verificationChecks.create({
+            to: `+91${phone}`,
+            code
+        });
+
+        if (data.status === 'approved') {
+
+            const user = await User.findOne({ phone });
+
+            if (user) {
+                const token = await createToken(user);
+
+                res.cookie('INUNDATION', token, { httpOnly: true, maxAge: maxAge * 1000 });
+
+                return res.status(400).json({
+                    user,
+                    error: 'This phone is already registered!'
+                });
+            } else {
+                const newUser = new User({
+                    phone,
+                    name,
+                    role,
+                    default_loc
+                });
+                await newUser.save();
+
+                const token = await createToken(newUser);
+                res.cookie('INUNDATION', token, { httpOnly: true, maxAge: maxAge * 1000 });
+
+                return res.json({
+                    user: newUser,
+                    success: 'User authenticated successfully!'
+                });
+            }
+        } else {
+            return res.json({
+                error: 'Please input the correct OTP!'
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.json({ error: 'Some error occured!' });
+    }
+};
--- website/routes/auth.js
+++ website/routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-const { phone_signup, verifyOTP, get_all_users, get_user_by_phone_no } = require('../controllers/auth');
+const { phone_signup, verifyOTP, get_all_users, get_user_by_phone_no, admin_phone_signup, admin_otp_verify } = require('../controllers/auth');
 
 // @route   POST api/signup
 // @desc    Send OTP to the user's phone number
@@ -10,7 +10,7 @@ router.post('/signup', phone_signup);
 
 // @route   POST api/verifyOTP
 // @desc    Verify the OTP put by the user
-//           if the OTP is okay, save the data from the user to mongodb database
+//           if the OTP is okay, save the data from the user to database
 //           otherwise, show error
 // @access  Public
 router.post('/verifyOTP', verifyOTP);
@@ -24,5 +24,17 @@ router.get('/get/all/users', get_all_users);
 // @desc    Get user by phone number
 // @access  Public
 router.get('/get/user/:phone', get_user_by_phone_no);
+
+// @route   GET api/admin/signup
+// @desc    Send OTP to the admin's phone number
+// @access  Private
+router.post('/admin/signup', admin_phone_signup);
+
+// @route   GET api/admin/verifyOTP
+// @desc    Verify the OTP put by the admin
+//           if the OTP is okay, save the data from the admin to database
+//           otherwise, show error
+// @access  Private
+router.post('/admin/verifyOTP', admin_otp_verify);
 
 module.exports = router;
